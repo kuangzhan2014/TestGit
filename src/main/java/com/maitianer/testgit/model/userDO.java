931be22de22054286211bcdad270fcd29b76dae2
kuangzhan2014 <742598832@qq.com>
2019-06-04 10:38:21 +0800

    用户类

---
+++ src/main/java/com/maitianer/testgit/model/userDO.java
@@ -0,0 +1,26 @@
+package com.maitianer.testgit.model;
+
+/**
+ * @Author: zhou
+ * @Date: 2019/06/04 10:37
+ */
+public class userDO {
+    private String userName;
+    private Integer ID;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+}
