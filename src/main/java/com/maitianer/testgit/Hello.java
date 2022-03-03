4c8736fff8db2ac81c9677d51d6665b54b8197c7
kuangzhan2014 <742598832@qq.com>
2022-03-03 10:54:11 +0800

    测试

--- src/main/java/com/maitianer/testgit/Hello.java
+++ src/main/java/com/maitianer/testgit/Hello.java
@@ -5,4 +5,7 @@ package com.maitianer.testgit;
  * @Date: 2019/06/04 10:00
  */
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("测试git的push功能");
+    }
 }
